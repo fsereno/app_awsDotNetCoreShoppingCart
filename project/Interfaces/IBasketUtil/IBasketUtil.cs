6b76d529d4a2fa055d32fa915a508bd5b9b27ab1
Fabio Sereno <fabio_sereno@hotmail.com>
2020-11-13 21:52:39 +0000

    Added folders

---
+++ project/Interfaces/IBasketUtil/IBasketUtil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Models;
+
+namespace Utils
+{
+    public interface IBasketUtil
+    {
+        bool TryRange(int index, IList<Item> collection, out int position);
+
+        List<Item> GetItems(List<Item> requestItems, List<Item> localItems);
+    }
+}
---
+++ project/Models/Item.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Models
+{
+    public class Item
+    {
+        public Item() {
+            this.Name = string.Empty;
+        }
+        public string Name { get; set; }
+    }
+}
